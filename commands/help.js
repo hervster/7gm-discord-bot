10fe4754a57fc629cb64caeaccf22004495078c6
hervster <hervenyemeck@gmail.com>
2021-09-13 19:58:15 -0400

    Added embed to help, added resources folder for images/vids

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,27 @@
+const { MessageEmbed, Message } = require('discord.js');
+
+const helpEmbed = new MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('List of Commands')
+    .setURL('https://github.com/hervster/7gm-discord-bot/blob/main/commands.js')
+    .setAuthor('Herve Nyemeck (Aleph)', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+    .setDescription('Command list for the Good Man Bot in the 7 Good Men (With Families) Discord Server')
+    .setThumbnail('./Resources/Images/2Face.png')
+    .addFields(
+        { name: 'Commands', value: 'All Available Commands' },
+        { name: '\u200B', value: '\u200B' },
+        { name: '!vid {name}', value: 'Retrieve a video with the given name', inline: true },
+        { name: '!gif {name}', value: 'Retrieve a gif with the given name', inline: true },
+        { name: '!img {name}', value: 'Retrieve an image with the given name', inline: true },
+        { name: '!list {name}', value: 'Retrieve a list of all files for a given command', inline: true },
+    )
+    .setImage('./Resources/Images/Gifs/Sas.gif')
+    .setTimestamp()
+    .setFooter('Personal Use Only', 'https://i.imgur.com/AfFp7pu.png');
+
+
 module.exports = async function (msg, args)
 {
     console.log("Im helpful");
-    msg.channel.send("Im helpful!");
+    msg.channel.send( { embeds: [helpEmbed] } );
 }
--- commands/list.js
+++ commands/list.js
@@ -1,5 +1,26 @@
+const { MessageEmbed, Message } = require('discord.js');
+
+const tempEmbed = new MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('List of Commands')
+    .setURL('https://github.com/hervster/7gm-discord-bot/blob/main/commands.js')
+    .setAuthor('Herve Nyemeck (Aleph)', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+    .setDescription('List of commands for using the Good Man Bot in the Seven Good Men (With Families) Discord Channel')
+    .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+    .addFields(
+        { name: 'Regular field title', value: 'Some value here' },
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+    )
+    .addField('Inline field title', 'Some value here', true)
+    .setImage('https://i.imgur.com/AfFp7pu.png')
+    .setTimestamp()
+    .setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
+
+
 module.exports = function (msg)
 {
     console.log("Im a list");
-    msg.channel.send("Im a list!");
+    msg.channel.send( { embeds: [tempEmbed] } );
 }
